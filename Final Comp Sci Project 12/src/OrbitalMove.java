df9ba49b6cf90bc83c7b44c4ee398b2e87728adc
Nicole <nicole@nicoles-mbp>
2019-01-12 09:02:40 -0500

    old code

---
+++ Final Comp Sci Project 12/src/OrbitalMove.java
@@ -0,0 +1,29 @@
+class OrbitalMove{
+	private int orbitalX; //movement relative to center of circle
+	private int orbitalY;
+	private double radian;
+	
+	OrbitalMove(int orbitalX, int orbitalY){
+		this.orbitalX=orbitalX;
+		this.orbitalY=orbitalY;
+	}
+	public int getOrbitalX() {
+		return this.orbitalX;
+	}
+	public void setOrbitalX(int orbitalX) {
+		this.orbitalX=orbitalX;
+	}
+	public int getOrbitalY() {
+		return this.orbitalY;
+	}
+	public void setOrbitalY(int orbitalY) {
+		this.orbitalY=orbitalY;
+	}
+	public double getRadian() {
+		return this.radian;
+	}
+	public void setRadian(double radian) {
+		this.radian=radian;
+	}
+	
+}
--- Final Comp Sci Project 12/src/Sun.java
+++ Final Comp Sci Project 12/src/Sun.java
@@ -1,10 +1,15 @@
 import java.awt.Color;
+import java.awt.Graphics;
 
 class Sun extends SpaceObject{
-	Sun(){	}
 	
 	Sun(String name, double daysRotates, double angleOfTilt, String directionRotates, double radius, Color color){
 		super(name, daysRotates, angleOfTilt, directionRotates, radius, color);
+	}
+	public void drawSun(Graphics g,int centerX, int centerY, int backgroundX, int backgroundY, double ratioP) {
+		 int radius2=(int)(radius/ratioP);
+		  g.setColor(color);
+		  g.fillOval(centerX+backgroundX,centerY-radius2+backgroundY,radius2*2, radius2*2);		 
 	}
 	
 }
